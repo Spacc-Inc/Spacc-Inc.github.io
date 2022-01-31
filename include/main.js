77b5733e6fa020e5d81a37016ad3d4c261220624
andrigamerita <37557992+andrigamerita@users.noreply.github.com>
2022-01-31 23:57:52 +0100

    move css

---
+++ include/main.js
@@ -0,0 +1,9 @@
+/*
+function startRandomizer(){
+	window.setTimeout( function(){
+		window.alert("Hello World " + Math.random() * 1000);
+		startRandomizer();
+	}, Math.random() * 10000);
+}
+startRandomizer();
+*/
